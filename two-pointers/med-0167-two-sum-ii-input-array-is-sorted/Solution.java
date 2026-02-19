ff073b28b3239bd4dffaf9450a382b6cdbf39ee7
Aadarsh P P <aadarshaaadrshpp@gmail.com>
2026-02-19 16:22:02 +0530

    Add LeetCode 167 - Two Sum II - Input Array Is Sorted

---
+++ two-pointers/med-0167-two-sum-ii-input-array-is-sorted/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while (i < j) {
+            int currSum = numbers[i] + numbers[j];
+
+            if (currSum == target) return new int[]{i + 1, j + 1};
+            else if (currSum < target) i++;
+            else j--;
+        }
+
+        return new int[]{-1, -1};
+    }
+}
---
+++ two-pointers/med-0167-two-sum-ii-input-array-is-sorted/Solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+
+        while (i < j):
+            curr_sum = numbers[i] + numbers[j]
+
+            if curr_sum < target:
+                i += 1
+            elif curr_sum > target:
+                j -= 1
+            else:
+                return [i + 1, j + 1]
+
+        return [-1, -1]
