dff46b4338f2d7aa2f0e0581015dd4f170e455b7
Aadarsh P P <aadarshaaadrshpp@gmail.com>
2026-02-26 00:09:12 +0530

    Update LeetCode 739 - Daily Temperatures (Solution)

--- 04-stack/med-0739-daily-temperatures/Solution.java
+++ 04-stack/med-0739-daily-temperatures/Solution.java
@@ -2,14 +2,15 @@ class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
         int[] result = new int[n];
-        Stack<Integer> stack = new Stack<>(); 
+        int[] stack = new int[n]; 
+        int top = -1;              
 
         for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int prevIndex = stack.pop();
+            while (top >= 0 && temperatures[i] > temperatures[stack[top]]) {
+                int prevIndex = stack[top--]; 
                 result[prevIndex] = i - prevIndex;
             }
-            stack.push(i);
+            stack[++top] = i; 
         }
 
         return result;
